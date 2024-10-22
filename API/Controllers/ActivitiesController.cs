64f71fc8395957cce00ae123d4b86c7d4ef2a26b
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-10-22 21:00:27 +0300

    Account Controller and Token Service added and configured to use JWT

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,18 +1,19 @@
 ﻿using Application.Activities;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        [HttpGet] //api/activities
+        [HttpGet]
         public async Task<IActionResult> GetActivities()
         {
             return HandleResult(await Mediator.Send(new List.Query()));
         }
 
-        [HttpGet("{id}")] //api/activities/someid
+        [HttpGet("{id}")]
         public async Task<IActionResult> GetActivity(Guid id)
         {
             return HandleResult(await Mediator.Send(new Details.Query { Id = id }));
