bdd439e2651ff054598f1d96ed10e84747f73fb6
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-10-23 21:09:17 +0300

    Client side Login and Registration implemented

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -14,5 +14,5 @@ createRoot(document.getElementById('root')!).render(
     <StoreContext.Provider value={store}>
       <RouterProvider router={router} />
     </StoreContext.Provider>
-  </StrictMode>,
+  </StrictMode>
 )
