d633d765b78efd10b00e42a14925c231cfdfd5f4
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-11-04 20:46:30 +0200

    Comments validation added and issues fixed

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -12,12 +12,13 @@ import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
 export default observer(function ActivityDetails() {
 
     const { activityStore } = useStore();
-    const { selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+    const { selectedActivity: activity, loadActivity, loadingInitial, clearSelectedActivity} = activityStore;
     const {id} = useParams();
 
     useEffect(() => {
-        if (id) loadActivity(id)
-    }, [id, loadActivity])
+        if (id) loadActivity(id);
+        return () => clearSelectedActivity();
+    }, [id, loadActivity, clearSelectedActivity])
 
     if (!activity || loadingInitial) {
         return <LoadingComponents />
