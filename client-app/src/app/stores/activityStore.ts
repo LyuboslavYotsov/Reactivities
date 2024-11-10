fba177041127bdd123466fa920804fd0e69b6991
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-11-10 17:49:35 +0200

    Client side filtering implemented

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable, runInAction } from "mobx";
+import { makeAutoObservable, reaction, runInAction } from "mobx";
 import { Activity, ActivityFormValues } from "../models/activity";
 import agent from "../api/agent";
 import { v4 as uuid } from "uuid";
@@ -15,19 +15,61 @@ export default class ActivityStore {
   loadingInitial = false;
   pagination: Pagination | null = null;
   pagingParams = new PagingParams();
+  predicate = new Map().set('all', true);
 
   constructor() {
     makeAutoObservable(this);
+
+    reaction(
+      () => this.predicate.keys(),
+      () => {
+        this.pagingParams = new PagingParams();
+        this.activityRegistry.clear();
+        this.loadActivities();
+      }
+    )
   }
 
   setPagingParams = (pagingParams: PagingParams) => {
     this.pagingParams = pagingParams;
-  }
+  };
+
+  setPredicate = (predicate: string, value: string | Date) => {
+    const resetPredicate = () => {
+      this.predicate.forEach((_value, key) => {
+        if (key !== 'startDate') this.predicate.delete(key);
+      });
+    };
+    switch (predicate) {
+      case 'all':
+        resetPredicate();
+        this.predicate.set('all', true);
+        break;
+      case 'isGoing':
+        resetPredicate();
+        this.predicate.set('isGoing', true);
+        break;
+      case 'isHost':
+        resetPredicate();
+        this.predicate.set('isHost', true);
+        break;
+      case 'startDate':
+        this.predicate.delete('startDate');
+        this.predicate.set('startDate', value);
+    }
+  };
 
   get axiosParams() {
     const params = new URLSearchParams();
     params.append('pageNumber', this.pagingParams.pageNumber.toString());
     params.append('pageSize', this.pagingParams.pageSize.toString());
+    this.predicate.forEach((value, key) => {
+      if (key === 'startDate') {
+        params.append(key, (value as Date).toISOString());
+      } else {
+        params.append(key, value);
+      }
+    });
     return params;
   }
 
@@ -40,7 +82,7 @@ export default class ActivityStore {
   get groupedActivities() {
     return Object.entries(
       this.activitiesByDate.reduce((activities, activity) => {
-        const date = format(activity.date!, "dd MMM yyyy");
+        const date = format(activity.date!, 'dd MMM yyyy');
         activities[date] = activities[date]
           ? [...activities[date], activity]
           : [activity];
@@ -68,7 +110,7 @@ export default class ActivityStore {
 
   setPagination = (pagination: Pagination) => {
     this.pagination = pagination;
-  }
+  };
 
   loadActivity = async (id: string) => {
     let activity = this.getActivity(id);
@@ -139,7 +181,10 @@ export default class ActivityStore {
       await agent.Activities.update(activity);
       runInAction(() => {
         if (activity.id) {
-          const updatedActivity = {...this.getActivity(activity.id), ...activity};
+          const updatedActivity = {
+            ...this.getActivity(activity.id),
+            ...activity,
+          };
           this.activityRegistry.set(activity.id, updatedActivity as Activity);
           this.selectedActivity = updatedActivity as Activity;
         }
@@ -177,22 +222,27 @@ export default class ActivityStore {
       await agent.Activities.attend(this.selectedActivity!.id);
       runInAction(() => {
         if (this.selectedActivity?.isGoing) {
-          this.selectedActivity.attendees = 
-               this.selectedActivity.attendees?.filter(a => a.username !== user?.username);
+          this.selectedActivity.attendees =
+            this.selectedActivity.attendees?.filter(
+              (a) => a.username !== user?.username
+            );
           this.selectedActivity.isGoing = false;
         } else {
-            const attendee = new Profile(user!);
-            this.selectedActivity?.attendees?.push(attendee);
-            this.selectedActivity!.isGoing = true;
+          const attendee = new Profile(user!);
+          this.selectedActivity?.attendees?.push(attendee);
+          this.selectedActivity!.isGoing = true;
         }
-        this.activityRegistry.set(this.selectedActivity!.id, this.selectedActivity!)
-      })
+        this.activityRegistry.set(
+          this.selectedActivity!.id,
+          this.selectedActivity!
+        );
+      });
     } catch (error) {
       console.log(error);
     } finally {
-      runInAction(() => this.loading = false);
+      runInAction(() => (this.loading = false));
     }
-  }
+  };
 
   cancelActivityToggle = async () => {
     this.loading = true;
@@ -200,28 +250,34 @@ export default class ActivityStore {
     try {
       await agent.Activities.attend(this.selectedActivity!.id);
       runInAction(() => {
-        this.selectedActivity!.isCancelled = !this.selectedActivity?.isCancelled;
-        this.activityRegistry.set(this.selectedActivity!.id, this.selectedActivity!)
-      })
+        this.selectedActivity!.isCancelled =
+          !this.selectedActivity?.isCancelled;
+        this.activityRegistry.set(
+          this.selectedActivity!.id,
+          this.selectedActivity!
+        );
+      });
     } catch (error) {
       console.log(error);
     } finally {
-      runInAction(() => this.loading = false);
+      runInAction(() => (this.loading = false));
     }
-  }
+  };
 
   clearSelectedActivity = () => {
     this.selectedActivity = undefined;
-  }
+  };
 
   updateAttendeeFollowing = (username: string) => {
-    this.activityRegistry.forEach(activity => {
-      activity.attendees.forEach(attendee => {
+    this.activityRegistry.forEach((activity) => {
+      activity.attendees.forEach((attendee) => {
         if (attendee.username === username) {
-          attendee.following ? attendee.followersCount-- : attendee.followersCount++;
+          attendee.following
+            ? attendee.followersCount--
+            : attendee.followersCount++;
           attendee.following = !attendee.following;
         }
-      })
-    })
-  }
+      });
+    });
+  };
 }
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,16 +3,16 @@ import ActivityList from "./ActivityList";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import { useEffect, useState } from "react";
-import LoadingComponent from "../../../app/layout/LoadingComponent";
 import ActivityFilters from "./ActivityFilters";
 import { PagingParams } from "../../../app/models/pagination";
 import InfiniteScroll from "react-infinite-scroller";
+import ActivityListItemPlaceholder from "./ActivityListItemPlaceholder";
 
 
-export default observer( function ActivityDashboard() {
+export default observer(function ActivityDashboard() {
 
   const { activityStore } = useStore();
-  const { loadActivities, activityRegistry, setPagingParams, pagination} = activityStore;
+  const { loadActivities, activityRegistry, setPagingParams, pagination } = activityStore;
   const [loadingNext, setLoadingNext] = useState(false);
 
   function handleGetNex() {
@@ -20,24 +20,29 @@ export default observer( function ActivityDashboard() {
     setPagingParams(new PagingParams(pagination!.currentPage + 1))
     loadActivities().then(() => setLoadingNext(false));
   }
-  
+
   useEffect(() => {
     if (activityRegistry.size <= 1) loadActivities();
   }, [loadActivities, activityRegistry.size]);
 
-  if (activityStore.loadingInitial && !loadingNext) return <LoadingComponent content='Loading activities...' />
-
   return (
     <Grid>
       <Grid.Column width={'10'}>
-        <InfiniteScroll
-          pageStart={0}
-          loadMore={handleGetNex}
-          hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
-          initialLoad={false}
-        >
-          <ActivityList />
-        </InfiniteScroll>
+        {activityStore.loadingInitial && activityRegistry.size === 0 && !loadingNext ? (
+          <>
+            <ActivityListItemPlaceholder />
+            <ActivityListItemPlaceholder />
+          </>
+        ) : (
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={handleGetNex}
+            hasMore={!loadingNext && !!pagination && pagination.currentPage < pagination.totalPages}
+            initialLoad={false}
+          >
+            <ActivityList />
+          </InfiniteScroll>
+        )}
       </Grid.Column>
       <Grid.Column width={'6'}>
         <ActivityFilters />
